93c38aafafdab24b10cb0e80319c273baa83c20b
Mike <mikhail.yakhyayeu@gmail.com>
2023-10-01 15:56:58 +0300

    scripts for dev and prod env

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -9,7 +9,7 @@ import { BuildOptions } from "./types/config";
 import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode,
@@ -24,7 +24,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
-        devtool: 'inline-source-map',
-        devServer: buildDevServer(options)
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     };
 }
