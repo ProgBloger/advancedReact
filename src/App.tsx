e0c8aee2b3e050bdb142b67fae9a375b333f9f2c
Mike <mikhail.yakhyayeu@gmail.com>
2023-10-01 18:29:31 +0300

    styles configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Counter} from './components/Counter';
+import './index.scss';
+
+const App = () => {
+    return (
+        <div className='app'>
+            This is Sparta-a-a-a!
+            <Counter />
+        </div>
+    )
+}
+
+export default App
