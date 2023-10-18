68cee9bc89e1ae830bea844e718c02953a3d129d
Mike <mikhail.yakhyayeu@gmail.com>
2023-10-18 20:29:22 +0300

    navbar. color styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
-import { AboutPage } from 'pages/AboutPage/index';
-import { MainPage } from 'pages/MainPage/index';
 import { useTheme } from 'app/providers/ThemeProvider/index';
 import { classNames } from 'shared/lib/classNames';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage />} />
-                <Route path={'/'} element={<MainPage />} />
-            </Routes>
         </div>
     )
 }
